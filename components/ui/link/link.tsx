9e2cb0766129f2a7ba2e4fb990c967beccb19fa5
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2023-08-21 06:05:01 +0000

    fix(components/ui): types

--- components/ui/link/link.tsx
+++ components/ui/link/link.tsx
@@ -1,7 +1,8 @@
-import NextLink from "next/link";
+import NextLink, { LinkProps } from "next/link";
 import classes from "./link.module.css";
+import { ReactNode } from "react";
 
-export default function Link({ href = "", children = null, ...rest }) {
+export default function Link({ href = "", children = null, ...rest } : LinkProps & {children: ReactNode}) {
  return (
   <NextLink href={href} {...rest}>
    <a className={classes.link}>
